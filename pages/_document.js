8671f491b2ccfb7d8b17f41460672bacffce7c5b
ruben-martin-lozano <ruben.martin.lozano@gmail.com>
2023-05-02 21:46:13 +0200

    cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
